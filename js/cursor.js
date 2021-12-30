98cef44ebd83216e46628dda59141c5215bdbe6a
Kunal Marwaha <marwahaha@berkeley.edu>
2021-12-30 03:02:50 -0600

    Update

--- js/cursor.js
+++ js/cursor.js
@@ -3,8 +3,9 @@ var gestureTimeoutID;
 var periodicGesturesTimeoutID;
 
 window.simulateRandomGesture = function (doneCallback) {
-    var target = document.querySelector('canvas');
+    var t = 0;
 
+    var target = document.querySelector('canvas');
     var rect = target.getBoundingClientRect();
 
     var simulateMouseEvent = function simulateMouseEvent(type, point) {
@@ -19,19 +20,21 @@ window.simulateRandomGesture = function (doneCallback) {
         target.dispatchEvent(event);
     };
 
-    var t = 0;
-
     // More fun:
     var cx = Math.random() * rect.width;
     var cy = Math.random() * rect.height;
     var gestureComponents = [];
-    var numberOfComponents = 5;
+    var numberOfComponents = 10;
+    var rx = (1.2 + Math.random()) * Math.min(rect.width, rect.height) / 2 / numberOfComponents;
+    var ry = (1.2 + Math.random()) * Math.min(rect.width, rect.height) / 2 / numberOfComponents;
+    angularFactor =2*(Math.random() - 0.5);
+    angularOffset = 3*(Math.random() - 0.5);
     for (var i = 0; i < numberOfComponents; i += 1) {
         gestureComponents.push({
-            rx: Math.random() * Math.min(rect.width, rect.height) / 2 / numberOfComponents,
-            ry: Math.random() * Math.min(rect.width, rect.height) / 2 / numberOfComponents,
-            angularFactor: Math.random() * 5 - Math.random(),
-            angularOffset: Math.random() * 5 - Math.random()
+            rx: rx,
+            ry: ry,
+            angularFactor: angularFactor,
+            angularOffset: angularOffset,
         });
     }
     var getPointAtTime = function getPointAtTime(t) {
@@ -44,12 +47,12 @@ window.simulateRandomGesture = function (doneCallback) {
         return point;
     };
 
-
-    simulateMouseEvent('mousedown', getPointAtTime(t));
+    simulateMouseEvent('mousedown', getPointAtTime(-1));
+    simulateMouseEvent('mouseup',  getPointAtTime(0));
     var move = function move() {
-        t += 1;
-        if (t > 5) {
-            simulateMouseEvent('mouseup', getPointAtTime(t));
+        end = (15);
+        if (t > end) {
+            // simulateMouseEvent('mouseup', getPointAtTime(t));
             if (doneCallback) {
                 doneCallback();
             }
@@ -57,12 +60,13 @@ window.simulateRandomGesture = function (doneCallback) {
             simulateMouseEvent('mousemove', getPointAtTime(t));
             gestureTimeoutID = setTimeout(move, 10);
         }
+        t += 1;
     };
     move();
 };
 
 window.simulateRandomGesturesPeriodically = function (delayBetweenGestures) {
-    delayBetweenGestures = delayBetweenGestures !== undefined ? delayBetweenGestures : 500;
+    delayBetweenGestures = delayBetweenGestures !== undefined ? delayBetweenGestures : 150;
 
     var waitThenGo = function waitThenGo() {
         periodicGesturesTimeoutID = setTimeout(function () {
